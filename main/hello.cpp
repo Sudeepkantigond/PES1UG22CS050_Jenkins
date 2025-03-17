bba51230857c2f6c1c26707d4aa43353da96b9c2
Sudeepkantigond <158493678+Sudeepkantigond@users.noreply.github.com>
2025-03-17 18:12:58 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main(void) {
+  cout << "************\n";
+  cout << "Hey ,I am Ajay Sawale\n";
+  cout << "************\n";
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
